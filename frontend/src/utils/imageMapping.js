c3d47cd6bbd2d49679bb3b5a1fcae1b18950c974
Javier Santos <javsanmar5@alum.us.es>
2024-11-12 16:58:19 +0100

    fix: Refactor sport image mapping to separate file

---
+++ frontend/src/utils/imageMapping.js
@@ -0,0 +1,11 @@
+import baloncesto_image from './../assets/images/baloncesto.png';
+import futbol_image from './../assets/images/futbol.png';
+import padel_image from './../assets/images/padel.png';
+import piscina_image from './../assets/images/piscina.png';
+
+export const sportImages = {
+    'Padel': padel_image,
+    'Fútbol': futbol_image,
+    'Baloncesto': baloncesto_image,
+    'Piscina': piscina_image
+};
