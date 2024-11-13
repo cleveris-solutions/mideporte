adce90ee739b0360755ebcf9c3475eb19e0b2554
JosueRodLop <josrodlop19@alum.us.es>
2024-11-13 17:22:40 +0100

    feat: Create booking model

---
+++ backend/booking/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class reservation(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    start = models.DateTimeField(null=False)
+    end = models.DateTimeField(null=False)
+    cancelled = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return self.id
