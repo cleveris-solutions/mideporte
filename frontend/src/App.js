08ef87890585501102d9007467a031ac3aa88520
Javier Santos <javsanmar5@alum.us.es>
2024-11-15 20:36:56 +0100

    feat: Adding authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,34 @@ import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Home from './screens/Home';
 import SportDetail from './screens/SportDetail';
+import Login from './screens/Login';
+import { useContext } from 'react';
+import { AuthContext } from './auth/AuthContext'; 
 
 function App() {
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+  
   return (
     <Router>
       <Navbar />
-      
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/deportes/:sportName" element={<SportDetail />} />
-        </Routes>
-      </div>
 
+      {user ? (
+        <>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/deportes/:sportName" element={<SportDetail />} />
+            </Routes>
+          </div>
+        </>
+      ) : (
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      )}
       <Footer />
     </Router>
   );
