657150661283d6dc1ca13bf3789a2f42daebb5cd
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2024-11-16 16:17:32 +0100

    feat: Add booking and installation URL configurations

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from installation import views as installation_views
-from booking import views as booking_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/sports', installation_views.sport_list, name='sport_list'),
-    path('api/v1/bookings', booking_views.create_booking, name='create_booking'),
-    path('api/v1/availableSchedule/<int:installation_id>/<str:date>', installation_views.available_schedule, name='available_schedule'),
+    path('api/v1/', include('installation.urls')),
+    path('api/v1/', include('booking.urls')),
 ]
