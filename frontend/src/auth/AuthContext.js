bc297be156a7dd95036f6ed619cc45e8883d71d3
Javier Santos <javsanmar5@alum.us.es>
2024-11-16 19:05:01 +0100

    feat: Add a way to log in without backend

--- frontend/src/auth/AuthContext.js
+++ frontend/src/auth/AuthContext.js
@@ -1,30 +1,34 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem('user');
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+    // THIS IS IMPORTANT:
+    // The commented code below allows the user to be stored in the local storage
+    // currently its not stored in purpose cause backend is not implemented yet
+    
+    // useEffect(() => {
+    //   const storedUser = localStorage.getItem('user');
+    //   if (storedUser) {
+    //     setUser(JSON.parse(storedUser));
+    //   }
+    // }, []);
+    
+    const login = (userData) => {
+        setUser(userData);
+        // localStorage.setItem('user', JSON.stringify(userData));
+    };
 
-  const login = (userData) => {
-    setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
-  };
+    const logout = () => {
+        setUser(null);
+        // localStorage.removeItem('user');
+    };
 
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-  };
-
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
