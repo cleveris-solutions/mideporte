230b3c057e1adeab4262da29403e6d2057e573f9
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2024-11-18 19:01:36 +0100

    feat: Add cancel booking functionality and improve error handling in booking views

--- backend/booking/urls.py
+++ backend/booking/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.create_booking, name='create_booking'),
+    path('cancel/<int:booking_id>', views.cancel_booking, name='cancel_booking'),   
 ]
--- backend/booking/views.py
+++ backend/booking/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render
 import json
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from .models import Booking
+from .models import Booking, BookingStatus
 from user.models import User
 from installation.models import Installation
 from rest_framework.decorators import api_view
 from drf_spectacular.utils import extend_schema,OpenApiParameter,OpenApiExample,OpenApiTypes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import permission_classes
+
+MESSAGE = 'message'         
+ERROR = 'error'
+ERROR_USER_NOT_FOUND = "User not found."
+ERROR_INSTALLATION_NOT_FOUND = "Installation not found."
+ERROR_BOOKING_NOT_FOUND = "Booking not found."
+ERROR_INVALID_DATA = "Invalid data. Please check the request payload."
+SUCCESS_BOOKING_CREATED = "Booking created successfully."
+SUCCESS_BOOKING_CANCELLED = "Booking cancelled successfully."
+
 @extend_schema()
 @require_http_methods(["POST"])
 @api_view(["POST"])
@@ -29,8 +39,24 @@ def create_booking(request):
                                          start_time=start_time,
                                          end_time=end_time)
     except User.DoesNotExist:
-        return JsonResponse({'error': 'User not found'}, status=404)
+        return JsonResponse({ERROR: ERROR_USER_NOT_FOUND}, status=404)
     except Installation.DoesNotExist:
-        return JsonResponse({'error': 'Installation not found'}, status=404)
+        return JsonResponse({ERROR: ERROR_INSTALLATION_NOT_FOUND}, status=404)
 
-    return JsonResponse({'message': 'Booking created successfully', 'booking_id':booking.id}, status=201)
+    return JsonResponse({ERROR: SUCCESS_BOOKING_CREATED, 'booking_id':booking.id}, status=201)
+
+
+#* Method to cancel a booking by changing its status to cancelled
+@extend_schema()
+@require_http_methods(["POST"])     
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def cancel_booking(request, booking_id):
+    try:
+        booking = Booking.objects.get(id=booking_id)
+        booking.status = BookingStatus.CANCELLED
+        booking.save()
+    except Booking.DoesNotExist:
+        return JsonResponse({ERROR: ERROR_BOOKING_NOT_FOUND}, status=404)
+
+    return JsonResponse({MESSAGE: SUCCESS_BOOKING_CANCELLED}, status=200)
