e7a29b84eb31677534c17aa9c281fd1482b58568
DavidFuentelsazRodriguez <davfuerod@alum.us.es>
2024-11-19 12:04:44 +0100

    feat: Update booking initial data and enhance booking creation and cancellation logic with improved error handling

--- backend/booking/models.py
+++ backend/booking/models.py
@@ -3,16 +3,15 @@ from user.models import User
 from installation.models import Installation
 
 class BookingStatus(models.TextChoices):
-    Scheduled = 'Programada'
-    Finished = 'Finalizada'
-    Cancelled = 'Cancelada'
+    Scheduled = 'Programada', 'Programada'
+    Finished = 'Finalizada', 'Finalizada'
+    Cancelled = 'Cancelada', 'Cancelada'
 
 class Booking(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='DNI')
     installation = models.ForeignKey(Installation, on_delete=models.CASCADE)
     start = models.DateTimeField(null=False)
-    end = models.DateTimeField(null=False)
     cancelled = models.BooleanField(default=False, null=False)
     status = models.CharField(max_length=20, choices=BookingStatus.choices, default=BookingStatus.Scheduled)
 
