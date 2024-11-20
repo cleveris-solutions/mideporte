795301db7ca2db4208cc44898c895eb4eeb79d2d
JosueRodLop <josrodlop19@alum.us.es>
2024-11-20 13:00:55 +0100

    feat: Add admin view to installations

--- backend/installation/admin.py
+++ backend/installation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Installation, InstallationType
 
-# Register your models here.
+@admin.register(Installation)
+class InstallationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'type', 'description', 'availability']
+    search_fields = ['name', 'type__name']
+
+@admin.register(InstallationType)
+class InstallationTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'image']
+    search_fields = ['name']
