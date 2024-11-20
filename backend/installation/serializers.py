890b6c1d2df3fcdac52d5d298c3b74d22e469690
JosueRodLop <josrodlop19@alum.us.es>
2024-11-20 12:56:26 +0100

    feat: Create get methon for installation

--- backend/installation/serializers.py
+++ backend/installation/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import InstallationType
+from .models import InstallationType, Installation
 
 class InstallationTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = InstallationType
         fields = ['id', 'name', 'description', 'image']
+        
+        
+class InstallationSerializer(serializers.ModelSerializer):
+    type = InstallationTypeSerializer()
+    class Meta:
+        model = Installation
+        fields = ['id', 'name', 'type', 'description', 'availability']
--- backend/installation/urls.py
+++ backend/installation/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('sports', views.sport_list, name='sport_list'),
     path('availableSchedule/<int:installation_id>/<str:date>', views.available_schedule, name='available_schedule'),
+    path('all', views.get_all_installations, name='get_all_installations'),
+    path('type/<str:type>', views.get_installations_by_type, name='get_installations_by_type'),
 ]
