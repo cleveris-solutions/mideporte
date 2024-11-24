3a801e27207372e6e5216a0589f76b3f6d86a44a
Javier Santos <javsanmar5@alum.us.es>
2024-11-24 13:37:15 +0100

    fix: Fix minor changes

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -1,24 +1,47 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import './../assets/styles/components/Schedule.css';
+import { AuthContext } from "../auth/AuthContext";
 
 const Schedule = ({ value, onChange , date, installationId}) => {
     const [availableHours, setAvailableHours] = useState([]);
+    const [everyHour, SetEveryHour] = useState([]);
     
-    const everyHour = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"];
+    // const everyHour = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"];
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
 
         const fetchAvailableHours = async () => {
             try {
-                const response = await fetch(`/api/v1/installations/availableSchedule/${installationId}/${date}`);
+                const response = await fetch(`/api/v1/installations/availableSchedule/${installationId}/${date}`, {
+                    headers: {
+                        'Authorization': `Token ${user.token}`
+                    }
+                });
                 const data = await response.json();
                 setAvailableHours(data);
             } catch (error) {
                 console.error("Error fetching data: ", error);
             }
         };
+
+
+        const fetchEveryHours = async () => {
+            try {
+                const response = await fetch(`/api/v1/installations/schedule/${installationId}/${date}`, {
+                    headers: {
+                        'Authorization': `Token ${user.token}`
+                    }
+                });
+                const data = await response.json();
+                SetEveryHour(data);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
     
         fetchAvailableHours();
+        fetchEveryHours();
         
     }, [date, installationId]);
 
