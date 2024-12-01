7ba52ffaba8e21cf04ce3dc8a4b728d392ca4ef2
Miguel Galán Lerate <miggaller@alum.us.es>
2024-12-01 18:07:13 +0100

    fix: frontend endpoints

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
 from django.views.generic import TemplateView
@@ -16,6 +16,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    re_path(r'^(?!api/v1/).*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
