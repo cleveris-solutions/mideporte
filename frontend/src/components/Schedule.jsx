422113fc56e9011acc52569a7ea682f4542dddac
Javier Santos <javsanmar5@alum.us.es>
2024-12-02 11:45:03 +0100

    fix: Remove cookies before login

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -6,7 +6,6 @@ const Schedule = ({ value, onChange , date, installationId}) => {
     const [availableHours, setAvailableHours] = useState([]);
     const [everyHour, SetEveryHour] = useState([]);
     
-    // const everyHour = ["09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"];
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
